cf79ebc8eb1bb58bf56178b8580de8789f8de6cc
Bulgaks <bulgaks92@gmail.com>
2023-11-04 17:29:16 +0300

    Добавляет адаптивную графику

--- source/js/script.js
+++ source/js/script.js
@@ -1,12 +1,6 @@
-let navMain = document.querySelector('.main-nav');
 let navToggle = document.querySelector('.main-header__toggle');
 
 navToggle.addEventListener('click', function () {
-  if (navToggle.classList.contains('main-header__toggle--closed')) {
-    navToggle.classList.remove('main-header__toggle--closed');
-    navToggle.classList.add('main-header__toggle--opened');
-  } else {
-    navToggle.classList.remove('main-header__toggle--opened');
-    navToggle.classList.add('main-header__toggle--closed');
-  }
+  navToggle.classList.toggle('main-header__toggle--closed');
+  navToggle.classList.toggle('main-header__toggle--opened');
 })
