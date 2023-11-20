07702cd6d86a52c3be0cbd57284abf61a96d11d1
Keks <keks@htmlacademy.ru>
2023-11-20 19:01:48 +0300

    :heavy_check_mark: Сборка #13

---
+++ 13/js/script.js
@@ -0,0 +1,1 @@
+let navToggle=document.querySelector(".main-header__toggle");navToggle.addEventListener("click",(function(){navToggle.classList.toggle("main-header__toggle--closed"),navToggle.classList.toggle("main-header__toggle--opened")}));
